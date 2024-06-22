c05a773d24c6fe919fa37ab640dcc6f18aa1500d
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2024-06-22 11:13:08 -0400

    Initial Commit

---
+++ Scope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scope
+//
+//  Created by Ari Reitman on 6/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
